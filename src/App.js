16d74a10306bf90eab3f6fea33d81788c2e9572f
bioroid17 <trace1997@ajou.ac.kr>
2026-04-30 16:18:26 +0900

    useEffect - useConfirm & usePreventLeave

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import { type } from "@testing-library/user-event/dist/type";
 import { useEffect, useRef, useState } from "react";
 
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    if (typeof onClick !== "function") {
-      return;
-    }
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
-    }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, []);
-  return typeof onClick !== "function" ? undefined : element;
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
 };
 
 export default function App() {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
+  const { enablePrevent, disablePrevent } = usePreventLeave();
   return (
     <div className="App">
-      <h1 ref={title}>Hi</h1>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
     </div>
   );
 }
