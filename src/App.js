b5dd4a62728676bf1bd77f0bf98a054e42182792
bioroid17 <trace1997@ajou.ac.kr>
2026-05-03 20:36:49 +0900

    useEffect - useAxios

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import { useEffect, useRef, useState } from "react";
-
-const useNotification = (title, options) => {
-  if (!("Notification" in window)) {
-    return;
-  }
-  const fireNotification = () => {
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          new Notification(title, options);
-        } else {
-          return;
-        }
-      });
-    } else {
-      new Notification(title, options);
-    }
-  };
-  return fireNotification;
-};
+import useAxios from "./useAxios";
 
 export default function App() {
-  const triggerNotification = useNotification("Can I steal your kimchi?", {
-    body: "I love kimchi don't you?",
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.bz/api/v2/list_movies.json",
   });
+
   return (
     <div className="App">
-      <button onClick={triggerNotification}>Hello</button>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 }
